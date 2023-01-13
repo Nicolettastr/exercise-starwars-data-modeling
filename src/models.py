5cc94c65f196068dfd0584fe858e9383cd30faff
Nicolettastr <nicolettastruggia@hotmail.com>
2023-01-13 20:08:07 +0000

    complete excersice

--- src/models.py
+++ src/models.py
@@ -8,23 +8,50 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Vehicles(Base):
+    __tablename__ = "vehicles"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Planet(Base):
+    __tablename__ = "planet"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Films(Base):
+    __tablename__ = "films"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Characters(Base):
+    __tablename__ = "characters"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    eye_color = Column(String(250))
+    hair_color = Column(String(250))
+    gender = Column(String(250))
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+    vehicles_id = Column(Integer, ForeignKey('vehicles.id'))
+    films_id = Column(Integer, ForeignKey('films.id'))
+    planet = relationship(Planet)
+    user = relationship(User)
+    vehicles = relationship(Vehicles)
+    films = relationship(Films)    
 
     def to_dict(self):
         return {}
