8d0fdb57c5c848912c749d901156af41e7d9cf06
Nicolettastr <nicolettastruggia@hotmail.com>
2023-02-28 10:58:45 +0000

    new diagram

--- src/models.py
+++ src/models.py
@@ -19,40 +19,42 @@ class Vehicles(Base):
     __tablename__ = "vehicles"
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    model = Column(String(256))
+    passengers = Column(String(256))
+    length = Column(String(256))
+    favorite_vehicle = relationship("Favorite")
 
 class Planet(Base):
     __tablename__ = "planet"
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    climate = Column(String(256))
+    population = Column(Integer)
+    orbital_period = Column(String(256))
+    rotation_period = Column(String(256))
+    diameter = Column(String(256))
+    favorite_planet = relationship("Favorite")
 
-class Films(Base):
-    __tablename__ = "films"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Characters(Base):
-    __tablename__ = "characters"
+class Character(Base):
+    __tablename__ = "character"
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     eye_color = Column(String(250))
     hair_color = Column(String(250))
     gender = Column(String(250))
+    favorite_character = relationship("Favorite")
 
 class Favorites(Base):
     __tablename__ = 'favorites'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    characters_id = Column(Integer, ForeignKey('characters.id'))
-    planets_id = Column(Integer, ForeignKey('planets.id'))
-    vehicles_id = Column(Integer, ForeignKey('vehicles.id'))
-    films_id = Column(Integer, ForeignKey('films.id'))
-    planet = relationship(Planet)
-    user = relationship(User)
-    vehicles = relationship(Vehicles)
-    films = relationship(Films)    
-
+    character_fav = Column(Integer,ForeignKey('character.id'))
+    planet_fav = Column(Integer,ForeignKey('planet.id'))
+    vehicle_fav = Column(Integer,ForeignKey('vehicle.id'))
+    character_relation = relationship ('Character', back_populates = 'favorite_character')
+    vehicle_relation = relationship ('Vehicle', back_populates = 'favorite_vehicle')
+    planet_relation = relationship ('Planet', back_populates = 'favorite_planet')
+    
     def to_dict(self):
         return {}
 
